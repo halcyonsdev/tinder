ac043802e28c74c472e39f56d07808a809c5340a
halcyonsdev <halcyon.ruslan@gmail.com>
2025-03-26 14:35:37 +0300

    Normalize line endings to LF

--- microservices/user-service/src/main/java/com/halcyon/tinder/userservice/model/support/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.halcyon.tinder.userservice.model.support;
-
-public enum Gender {
-    MALE,
-    FEMALE
-}
--- settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "tinder"
-include("microservices:user-service")
-findProject(":microservices:user-service")?.name = "user-service"
-include("common:redis-cache")
-findProject(":common:redis-cache")?.name = "redis-cache"
